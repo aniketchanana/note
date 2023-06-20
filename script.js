94c12723c478c19fdeb59c04273ce71999a6a8b0
aniket <aniket.chanana@gmail.com>
2023-06-20 23:57:59 +0530

    Some fixes

--- script.js
+++ script.js
@@ -1,50 +1,49 @@
-const DELTA = 64 * 2; // both side padding
-
 const LOCAL_STORAGE_KEYS = {
   NOTES: "notes",
   SIZE: "size",
 };
 
 let isFirstLoad = true;
+const notePad = document.getElementById("notepad");
 
 window.addEventListener("load", () => {
-  document.body.innerHTML = localStorage.getItem(LOCAL_STORAGE_KEYS.NOTES);
+  notePad.innerHTML = localStorage.getItem(LOCAL_STORAGE_KEYS.NOTES);
 
   const range = document.createRange();
   const sel = window.getSelection();
-  range.selectNodeContents(document.body);
+  range.selectNodeContents(notePad);
   range.collapse(false);
   sel.removeAllRanges();
   sel.addRange(range);
-  document.body.focus();
+  notePad.focus();
 });
-document.body.addEventListener("keydown", (e) => {
+notePad.addEventListener("keydown", (e) => {
   localStorage.setItem(LOCAL_STORAGE_KEYS.NOTES, e.target.innerHTML);
 });
 
 const resizeObserver = new ResizeObserver((entries) => {
   if (isFirstLoad) {
     let editorSize = localStorage.getItem(LOCAL_STORAGE_KEYS.SIZE);
-    document.body.style.opacity = "1";
     isFirstLoad = false;
     if (editorSize) {
       editorSize = JSON.parse(editorSize);
-      document.body.style.height = `${editorSize.height}px`;
-      document.body.style.width = `${editorSize.width}px`;
+      notePad.style.height = `${editorSize.height}px`;
+      notePad.style.width = `${editorSize.width}px`;
+    } else {
+      notePad.style.height = "100%";
+      notePad.style.width = "100%";
     }
+    notePad.style.opacity = "1";
     return;
   }
   const [entry] = entries;
-  const { width, height } = entry.contentRect;
+  const { inlineSize: width, blockSize: height } = entry.contentBoxSize[0];
 
-  const _height = height + DELTA;
-  const _width = width + DELTA;
-
-  document.body.style.height = _height;
-  document.body.style.width = _width;
+  notePad.style.height = height;
+  notePad.style.width = width;
   localStorage.setItem(
     LOCAL_STORAGE_KEYS.SIZE,
-    JSON.stringify({ height: _height, width: _width })
+    JSON.stringify({ height, width })
   );
 });
-resizeObserver.observe(document.body);
+resizeObserver.observe(notePad);
